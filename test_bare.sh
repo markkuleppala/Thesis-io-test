f8b5e7d07c0758ea1b10abc06f09001f429767a2
Markku Leppälä <38980504+markkuleppala@users.noreply.github.com>
2021-05-03 15:00:51 +0300

    Create test_bare.sh

---
+++ test_bare.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+mkdir test-volume results
+
+RW="read  write  randread  randwrite"
+
+for NUM_JOBS in 1 2 3
+do
+    echo -e 'Jobs num: $NUM_JOBS\n'		
+    for BS in 512 1024 2048 4096 8192 16384 32768 65536
+    do
+    	echo -e 'Block size: $BS\n'
+        for FIO_RW in $RW
+        do
+            echo -e 'IO pattern: $FIO_RW\n'
+            for i in 1 2 3
+            do
+                RUNTIME=30 FILESIZE=2G SIZE=2G NUM_JOBS=$NUM_JOBS FIO_RW=$FIO_RW \
+                    fio fio_jobfile.fio \
+                    --directory=/test-volume \
+                    --output-format=json+ \
+                    --blocksize=$BS \
+                    --output=/results/$VOLUME-bare-$BS-$NUM_JOBS-$FIO_RW-$i.json
+                rm -rf $DIR/*.dat
+            done
+        done
+    done
+done
